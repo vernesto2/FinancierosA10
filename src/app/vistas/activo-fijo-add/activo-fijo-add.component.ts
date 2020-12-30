a4cdab9d539afdde9d23087e17f96c63f61ba70c
mely96 <mely_esme24.96@hotmail.com>
2020-12-30 09:40:50 -0600

    modificamos un cambio de variable

--- src/app/vistas/activo-fijo-add/activo-fijo-add.component.ts
+++ src/app/vistas/activo-fijo-add/activo-fijo-add.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ActivoFijoModel } from 'app/models/ActivoFijo.model';
+import { ActivoFijoModel } from 'app/models/activoFijo.model';
 import { Unidad } from 'app/models/unidad.model';
 import { ActivoFijoService } from 'app/services/activo-fijo.service';
 @Component({

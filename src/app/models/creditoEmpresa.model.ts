f7ce98150ac61d927dd95ade19bdfb73e46b29f9
mely96 <mely_esme24.96@hotmail.com>
2021-01-11 12:19:36 -0600

    Agregado: todas las variables a los modelos

--- src/app/models/creditoEmpresa.model.ts
+++ src/app/models/creditoEmpresa.model.ts
@@ -1,1 +1,9 @@
-export class CreditoEmpresaModel {}
+import { EmpresaModel } from 'app/models/empresa.model';
+import { CreditoModel } from "./credito.model";
+
+export class CreditoEmpresaModel {
+    idCredito: number;
+    credito: CreditoModel;
+    empresa: EmpresaModel;
+    
+}
--- src/app/models/creditoPersonal.model.ts
+++ src/app/models/creditoPersonal.model.ts
@@ -1,1 +1,11 @@
-export class CreditoPersonalModel {}
+import { GarantiaFiadorModel } from './garantiaFiador.model';
+import { PersonaModel } from './persona.model';
+import { CreditoModel } from "./credito.model";
+
+export class CreditoPersonalModel {
+    idCredito: number;
+    credito: CreditoModel;
+    persona: PersonaModel;
+    garantiaFiador: Array<GarantiaFiadorModel>;
+    
+}

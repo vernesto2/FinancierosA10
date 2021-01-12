d05e4d15fa225354e4c072db1ef3eacb00e2e568
Jos <josepena2196@gmail.com>
2021-01-12 10:35:06 -0600

    cambios ya agrega detalle de activo fijo

--- src/app/models/activoFijo.model.ts
+++ src/app/models/activoFijo.model.ts
@@ -1,16 +1,7 @@
-import { Departamento } from "./departamento.mode";
 import { TipoActivo } from "./tipoActivo.model";
 
 export class ActivoFijoModel {
     id: number;
-    correlativo: number;
-    descripcion: string;
-    fechaAdquisicion: Date;
     nombre: string;
-    precioAdquisicion: number;
-    tipoAdquisicion: number;
-    vidaUtil: number;
-    departamento: Departamento;
     tipoActivo: TipoActivo;
-    codigoGenerado: string;
 }

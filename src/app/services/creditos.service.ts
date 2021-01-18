702a771ad3e34c67bbf0fccf6795324de9ad5dac
Ernesto <vacevedo242@gmail.com>
2021-01-17 22:16:15 -0600

    formato: ordene los Enspoint de creditoService.. ya estan separados por comentarios que indica a cual modulo pertenecen

--- src/app/services/creditos.service.ts
+++ src/app/services/creditos.service.ts
@@ -11,37 +11,58 @@ import { IngresoEgresoModel } from 'app/models/ingresoEgreso.model';
   providedIn: 'root'
 })
 export class CreditosService {
-  
+
   private base_uri = environment.base_uri;
-  
+
   constructor(private http: HttpClient) { }
 
+
+  //consultas de personas
   agregarCreditoPersona(credito: CreditoModel, tipoCredito: string, tipoTiempo: string): Observable<any> {
     if (tipoTiempo == 'año') {
       credito.tiempo = credito.tiempo * 12;
     }
-    console.log(credito.tiempo);
-    return this.http.post(`${this.base_uri}/credito/PERSONA/${tipoCredito}`, credito, { observe: 'response'});
+    return this.http.post(`${this.base_uri}/credito/PERSONA/${tipoCredito}`, credito, { observe: 'response' });
   }
 
+  comprobarIngresos(ingresosEgresosCliente: IngresoEgresoModel, ingresosEgresosFiador: IngresoEgresoModel): Observable<any> {
+    return this.http.post(`${this.base_uri}/credito/ingresosegresos`, { ingresosEgresosFiador, ingresosEgresosCliente }, { observe: 'response' });
+  }
+
+  listaCreditoPersonaEnCurso(): Observable<any> {
+    return this.http.get(`${this.base_uri}/credito/persona/encurso`, { observe: 'response' });
+  }
+  consultarSiPersonaPoseeCredito(dui: string): Observable<any> {
+    return this.http.get(`${this.base_uri}/credito/librecredito/PERSONA/${dui}`, { observe: 'response' });
+  }
+
+  //Consultas de empresas
   agregarCreditoEmpresa(credito: CreditoModel, tipoCredito: string, tipoTiempo: string): Observable<any> {
     if (tipoTiempo == 'año') {
       credito.tiempo = credito.tiempo * 12;
     }
-    console.log(credito.tiempo);
-    return this.http.post(`${this.base_uri}/credito/EMPRESA/${tipoCredito}`, credito, { observe: 'response'});
-  }
-
-  consultarSiPersonaPoseeCredito(dui: string): Observable<any> {
-    return this.http.get(`${this.base_uri}/credito/librecredito/PERSONA/${dui}`, {observe: 'response'});
+    return this.http.post(`${this.base_uri}/credito/EMPRESA/${tipoCredito}`, credito, { observe: 'response' });
   }
 
   consultarSiEmpresaPoseeCredito(nit: string): Observable<any> {
-    return this.http.get(`${this.base_uri}/credito/librecredito/EMPRESA/${nit}`, {observe: 'response'});
+    return this.http.get(`${this.base_uri}/credito/librecredito/EMPRESA/${nit}`, { observe: 'response' });
+  }
+
+  listaCreditoEmpresaEnCurso(): Observable<any> {
+    return this.http.get(`${this.base_uri}/credito/empresa/encurso`, { observe: 'response' });
+  }
+
+  buscarNitNombreEmpresa(buscar: string): Observable<any> {
+    return this.http.get(`${this.base_uri}/empresa/buscar/${buscar}`, { observe: 'response' });
+  }
+
+  //Consultas que se usan tanto en persona como empresa
+  obtenerBienPorCodigo(codigo: string): Observable<any> {
+    return this.http.get(`${this.base_uri}/biengarantia/${codigo}`, { observe: 'response' });
   }
 
   consultarSiBiengarantiaPoseeCredito(codigo: string): Observable<any> {
-    return this.http.get(`${this.base_uri}/credito/librecredito/BIENGARANTIA/${codigo}`, {observe: 'response'});
+    return this.http.get(`${this.base_uri}/credito/librecredito/BIENGARANTIA/${codigo}`, { observe: 'response' });
   }
 
   calcularPrecredito(monto: number, tiempo: number, tipoCredito: string, fecha: Date): Observable<any> {
@@ -49,33 +70,11 @@ export class CreditosService {
     const mes = fecha.getMonth(); // sacamos los meses actual
     const año = fecha.getFullYear(); // sacamos el año actual
 
-    let fechas = año + '-' + mes+1 + '-' + dias;
-    console.log(fechas);
-    return this.http.get(`${this.base_uri}/credito/precredito/${monto}/${tiempo}/${tipoCredito}/${fechas}`, { observe: 'response'});
+    let fechas = año + '-' + mes + 1 + '-' + dias;
+    return this.http.get(`${this.base_uri}/credito/precredito/${monto}/${tiempo}/${tipoCredito}/${fechas}`, { observe: 'response' });
   }
 
   rangoPolitica(): Observable<any> {
-    return this.http.get(`${this.base_uri}/politica/rangopolitica`,  { observe: 'response'});
-  }
-
-  comprobarIngresos(ingresosEgresosCliente: IngresoEgresoModel, ingresosEgresosFiador: IngresoEgresoModel): Observable<any> {
-    return this.http.post(`${this.base_uri}/credito/ingresosegresos`, {ingresosEgresosFiador, ingresosEgresosCliente}, {observe: 'response'});
-  }
-
-  listaCreditoPersonaEnCurso(): Observable<any> {
-    return this.http.get(`${this.base_uri}/credito/persona/encurso`, {observe: 'response'});
-  }
-
-  listaCreditoEmpresaEnCurso(): Observable<any> {
-    return this.http.get(`${this.base_uri}/credito/empresa/encurso`, {observe: 'response'});
-  }
-
-  //obtener un bien por su codigo
-  obtenerBienPorCodigo(codigo: string): Observable<any> {
-    return this.http.get(`${this.base_uri}/biengarantia/${codigo}`, {observe: 'response'});
-  }
-
-  buscarNitNombreEmpresa(buscar: string): Observable<any> {
-    return this.http.get(`${this.base_uri}/empresa/buscar/${buscar}`, {observe: 'response'});
+    return this.http.get(`${this.base_uri}/politica/rangopolitica`, { observe: 'response' });
   }
 }
